c8cd232d254f49abeb94ba6d7d8cc5e4641f6316
jim2107054 <mdjahid205hasan@gmail.com>
2026-06-27 15:59:44 +0600

    feat: initialize express server with Google GenAI SDK integration

---
+++ level4/server.js
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+import { GoogleGenAI } from "@google/genai"
+dotenv.config({ path: "../.env" })
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+});
+
+const main = async() => {
+    const response = await ai.models.generateContent({
+        model: "gemini-3.5-flash",
+        contents: "hello, what is 2+2. then 0/0",
+    })
+    console.log(response.text)
+}
+main()
+
+app.get("/", (req, res) => {
+    return res.json({message: "Hello from Level 4!"})
+})
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`)
+})
