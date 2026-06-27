5f5c5e45d3ac8c88f8b6b1f3c075a3aa314bb4b8
jim2107054 <mdjahid205hasan@gmail.com>
2026-06-27 16:59:34 +0600

    feat: initialize Express server with LangChain integration and weather tool support

--- level4/server.js
+++ level4/server.js
@@ -2,6 +2,9 @@ import express from "express"
 import dotenv from "dotenv"
 import { GoogleGenAI } from "@google/genai"
 dotenv.config({ path: "../.env" })
+import { createAgent, tool } from "langchain";
+import * as z from "zod";
+
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -9,44 +12,83 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY,
-});
+const getWeather = tool(
+  (input) => `It's always sunny in ${input.city}!`,
+  {
+    name: "get_weather",
+    description: "Get the weather for a given city",
+    schema: z.object({
+      city: z.string().describe("The city to get the weather for"),
+    }),
+  }
+);
 
-app.post("/chat", async(req, res) => {
-    const { input } = req.body || {}
+// const ai = new GoogleGenAI({
+//     apiKey: process.env.GEMINI_API_KEY,
+// });
+
+
+//! LangChain
+app.post("/chat", async(req, res)=> {
+    const {input} = req.body || {}
+
     if(!input){
         return res.status(400).json({message: "Input is required"})
     }
 
     try {
-        const response = await ai.models.generateContent({
-        model: "gemini-3.5-flash",
-        // contents: input,
-        contents: [
-            {
-                role: "system",
-                parts: [{
-                    text: "You are a math expert who answers in a concise and direct manner. and your name is Siraj sir. If you can't solve, say that it is beyond your ability."
-                }],
-            },
-            {
-                role: "user",
-                parts: [
-                    {
-                        text: input
-                    }
-                ]
-            }
-        ]
-    })
-    if (response && response.text) {
-        return res.json({message: response.text})
-    }
+        const agent = createAgent({
+      model: "google-genai:gemini-2.5-flash-lite",
+      tools: [getWeather],
+    });
+
+        const userContent = typeof input === "string" ? input : JSON.stringify(input);
+        const result = await agent.invoke({ messages: [{ role: "user", content: userContent }] });
+
+        const outputMessage = result.response || (result.messages && result.messages[result.messages.length - 1]?.content);
+        console.log(outputMessage); 
+        
+        res.json({ message: outputMessage }) 
     } catch (error) {
         return res.status(500).json({message: "Internal Server Error", error: error.message})
     }
-})  
+})
+
+//! Without langChain
+// app.post("/chat", async(req, res) => {
+//     const { input } = req.body || {}
+//     if(!input){
+//         return res.status(400).json({message: "Input is required"})
+//     }
+
+//     try {
+//         const response = await ai.models.generateContent({
+//         model: "gemini-3.5-flash",
+//         // contents: input,
+//         contents: [
+//             {
+//                 role: "system",
+//                 parts: [{
+//                     text: "You are a math expert who answers in a concise and direct manner. and your name is Siraj sir. If you can't solve, say that it is beyond your ability."
+//                 }],
+//             },
+//             {
+//                 role: "user",
+//                 parts: [
+//                     {
+//                         text: input
+//                     }
+//                 ]
+//             }
+//         ]
+//     })
+//     if (response && response.text) {
+//         return res.json({message: response.text})
+//     }
+//     } catch (error) {
+//         return res.status(500).json({message: "Internal Server Error", error: error.message})
+//     }
+// })  
 
 // const main = async() => {
 //     const response = await ai.models.generateContent({
